4f809c3056d3e4fa05632594b19c7e3bb6a982d5
seba-wetzel <seba.wetzel@gmail.com>
2024-05-15 00:41:51 -0300

    Fix: validaciones y guardado  de estado

--- src/app/(admin)/registrate/components/DireccionForm.tsx
+++ src/app/(admin)/registrate/components/DireccionForm.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { FormButton } from "@/components/FormButton";
 import Mapa from "@/components/Mapa";
 import { Button } from "@/components/ui/button";
 import {
@@ -13,38 +12,57 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useEffect, useState } from "react";
 
+import {
+  DireccionDefaultValues,
+  DireccionResolver,
+} from "@/modules/shared/adapters/ClienteSchema.zod";
 import { useForm } from "react-hook-form";
 
 export function DireccionForm() {
   const form = useForm({
-    defaultValues: {
-      calle: "",
-      numero: "",
-      barrio: "",
-      piso: "",
-      referencia: "",
-    },
+    resolver: DireccionResolver,
+    defaultValues: DireccionDefaultValues,
   });
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
+
+  useEffect(() => {
+    if (result) {
+      console.log(result);
+    }
+  }, [result]);
 
   const onSubmit = (data: any) => {
-    console.log({ data });
-    fetch("/api/validate", {
+    setLoading(true);
+    const clientData = window.localStorage.getItem("cliente") || "{}";
+    fetch("/api/registro", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify({
+        ...JSON.parse(clientData),
+        direccion: { ...data, provincia: "Buenos Aires", localidad: "Glew" },
+      }),
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setResult(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
       });
   };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="">
+        {error && <div className="text-red-500">{error}</div>}
         <div className="text-2xl flex flex-row gap-4 mb-4 flex-wrap justify-between">
           <FormField
             control={form.control}
@@ -78,6 +96,42 @@ export function DireccionForm() {
               </FormItem>
             )}
           />
+          {/* ENTRECALLES */}
+          <FormField
+            control={form.control}
+            name="entreCalles.calle1"
+            render={({ field }) => (
+              <FormItem className="flex flex-col flex-grow items-start w-full md:w-auto">
+                <FormLabel>Entre calles</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Entre calle 1" />
+                </FormControl>
+                {/* <FormDescription className="text-start ml-4">
+                  Numeracion
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="entreCalles.calle2"
+            render={({ field }) => (
+              <FormItem className="flex flex-col flex-grow items-start w-full md:w-auto">
+                <FormLabel className="text-transparent hidden md:block">
+                  .
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Entre calle 2" />
+                </FormControl>
+                {/* <FormDescription className="text-start ml-4">
+                  Numeracion
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
           <FormField
             control={form.control}
             name="barrio"
@@ -118,7 +172,7 @@ export function DireccionForm() {
                 <FormLabel>Referencia</FormLabel>
                 <FormControl>
                   <Textarea
-                    placeholder="Referencia"
+                    placeholder="Descripción de la fachada, puntos de referencia para encontrar la dirección, etc. "
                     className="resize-y"
                     {...field}
                   />
@@ -145,9 +199,12 @@ export function DireccionForm() {
           <Mapa />
         </div>
         <div className="mt-10 flex justify-around">
-          <FormButton className="bg-verde-secundario">
+          <Button
+            disabled={loading}
+            className="disabled:bg-verde-principal bg-verde-secundario"
+          >
             Validar dirección
-          </FormButton>
+          </Button>
           <Button
             type="button"
             className="disabled:bg-verde-principal bg-verde-secundario"
